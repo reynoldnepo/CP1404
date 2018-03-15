242ebaa34550ea8e6f265c869a8386350c816b10
jc429041 <reynoldnepomuceno@gmail.com>
2018-03-16 08:36:49 +1000

    Commitment test

--- odd_name.py
+++ odd_name.py
@@ -3,3 +3,9 @@ try:
 except TypeError:
     print("Invalid Name")
 print("Finished")
+
+import os
+print("The files and folders in {} are:".format(os.getcwd()))
+items = os.listdir('.')
+for item in items:
+ print(item)
