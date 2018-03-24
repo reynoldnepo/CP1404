c2253542e26cfe21ce406ab90559c1703331f1f4
reynoldnepo <37422844+reynoldnepo@users.noreply.github.com>
2018-03-24 13:49:57 +1000

    assignment(basic function without exceptions)

--- reynoldnepo_a1/assignment101.py
+++ reynoldnepo_a1/assignment101.py
@@ -4,30 +4,33 @@ Replace the contents of this module docstring with your own details.
 
 import csv
 
-movies_file = open('movies.csv')
-movies = csv.reader(movies_file, delimiter=',')
+input_movies = open('movies.csv',"r")
+movie_reader = csv.reader(input_movies, delimiter=',')
+output_movies = open('movies2.csv',"w")
+movie_writer = csv.writer(output_movies,delimiter=",", quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+#movies_writelist = csv.writer(movies_file, delimiter=',')
 
-
+#print(movies_readlist[-1])
 #List each line in readMovie =['Count','Movie','year','watched')
 #Add counter for sum_of_movies and determine if watched or not watched
 
-
+movies = []
+total_movies = 0
 
 def main():
-    print("Movies To Watch 1.0 - by Reynold Nepomuceno")
-    #print("{} movies loaded".format(sum_of_movies))#sum(1 for line in readMovie)
-    menu = """    L - List movies
-    A - Add a new movie
-    W - Watch a Movie
-    Q - Quit"""
-    print(menu)
+    create_movies()
 
+    print("Movies To Watch 1.0 - by Reynold Nepomuceno")
+    print("{} movies loaded".format(sum_of_movies))
+    menu = """\n L - List movies\n A - Add a new movie\n W - Watch a Movie\n Q - Quit"""
+    print(menu)
     choice = input(">>>").upper()
+
     while choice != "Q":
         if choice == "L":
             list_movie()
         elif choice == "A":
-            print('add_Qmovie()')
+            add_movie()
         elif choice == "W":
             print('movie_counter()')
         else:
@@ -35,14 +38,18 @@ def main():
         print(menu)
         choice = input(">>>".upper())
     print("Enjoy your movie!")
-    movies_file.close()
 
+    input_movies.close()
+    output_movies.close()
+
+def create_movies():
+    for row in movie_reader:
+        total_movies += 1
+        movies.append(row)
 
 def list_movie():
-    total_movies = 0
     total_watched = 0
     for row in movies:
-        total_movies += 1
         if row[3] == 'y':
             total_watched += 1
             watched = " "
@@ -50,16 +57,25 @@ def list_movie():
             watched = "*"
         print("{:}. {} {:<40} - {:>4} ({})".format(total_movies, watched, row[0], row[1], row[2]))
     print("{} movies watched,{} movies still to watch".format(total_watched,total_movies-total_watched))
+    print(movies)
 
 
 def add_movie():
     movie_title = input("Title of Movie:")
     movie_year = input("Year of Movie:")
-    movie_genre = input ("Genre of Movie:")
+    movie_genre = input("Genre of Movie:")
+    movie_watched = input("Have you watched this Movie? y or n")
+    new_movie = [movie_title, movie_year, movie_genre, movie_watched]
 
-    movies_file.write("{},{},{},n".format(movie_title,movie_year,movie_genre))
-    #if __name__ == '__main__'
+    movies.append(new_movie)
+    print(movies)
+    for item in movies:
+        movie_writer.writerow([item[0],item[1],item[2],item[3]])
 
-#main()
+#if __name__ == '__main__'
+
+
+main()
 #add_movie()
-list_movie()
+#list_movie()
+
