2410e305d65d46b009e3f90480461fb473eb00af
reynoldnepo <reynold.nepomuceno@gmail.com>
2018-04-06 09:24:18 +1000

    Practical 5 dictionaries

--- Practical5/string counter.py
+++ Practical5/string counter.py
@@ -1,14 +1,14 @@
 
 
-string_dic = {}
+string_count_dic = {}
 text = input("Write a sentece>").lower()
 words = text.split()
 
 for word in words:
-    if word in words:
-        string_dic[word] += 1
+    if word in string_count_dic:
+        string_count_dic[word] += 1
     else:
-        string_dic[word] = 1
+        string_count_dic[word] = 1
 
-for x,y in sorted(string_dic.items()):
-    print('{:{}} is {}'.format(x,len(x),y))
+for x,y in sorted(string_count_dic.items()):
+    print('{:} is {}'.format(x,y))
